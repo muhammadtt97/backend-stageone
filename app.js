79c13524e37acf4fd5a128cc89b848686fff754e
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-06 15:43:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ app.get('/api', (req, res) => {
     res.status(200).json(response);
 });
 
+dateFunction = () => {
+    day = new Date().getUTCDay();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const todays_day = daysOfWeek[day];
+
+    const now = new Date();
+    now.setMinutes(now.getUTCMinutes());
+    const todays_time = now.toISOString().slice(0, 19) + "Z"; // Adjusted UTC time
+
+    return [todays_day, todays_time];
+};
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
